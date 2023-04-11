8f9e4ef1f9ec79c00d71eb0aaae6c81a8fbb90a2
ThildaEles <thilda.eles@gmail.com>
2023-04-11 10:27:55 +0200

    Backend + Frontend work

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+    <ul>
+      <li *ngFor="let product of products">{{product.name}}</li>
+    </ul>
+  `,
 })
 export class AppComponent {
   title = 'frontend';
+  products: any[] | undefined ;
+  // products: string;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<string>('http://localhost:8080/api/products').subscribe(products => {
+      console.log(products);
+    });
+  }
 }
